51faff92df675aa7885e66c2771109392ea12c3d
Lorenzo Angelone <l.angelone@kapusons.it>
2024-08-09 12:18:05 +0200

    added backhandler

--- src/components/BottomSheetModalComponent.js
+++ src/components/BottomSheetModalComponent.js
@@ -1,10 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import {BackHandler} from 'react-native';
 import {
   BottomSheetModal,
   BottomSheetBackdrop,
 } from '@gorhom/bottom-sheet';
 
 const BottomSheetModalComponent = (props, ref) => {
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
   const renderBackdrop = useCallback( props => (
     <BottomSheetBackdrop
       {...props}
@@ -13,6 +16,23 @@ const BottomSheetModalComponent = (props, ref) => {
     />
   ), []);
 
+
+  const onBackPress = () => {
+    if (ref !== null) {
+      ref.current?.close();
+      return true;
+    }
+  };
+
+  useEffect(() => {
+    if (currentIndex !== -1) {
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }
+  }, [currentIndex]);
+
   return (
     <BottomSheetModal
       ref={ref}
@@ -20,7 +40,7 @@ const BottomSheetModalComponent = (props, ref) => {
       backdropComponent={renderBackdrop}
       snapPoints={props.snapPoints}
       onDismiss={() => !!props.onDismiss && props.onDismiss()}
-      onChange={(index) => !!props.onChange && props.onChange(index)}
+      onChange={(index) => {setCurrentIndex(index); !!props.onChange && props.onChange(index)}}
     >
       { props.content }
     </BottomSheetModal>
