d7533ada5bd9f705d317b1d1c7ea30e144b69829
Lorenzo Angelone <l.angelone@kapusons.it>
2024-09-09 16:44:17 +0200

    export button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as BottomSheetPicker} from './components/BottomSheetPickerComponent';
 export {default as BottomSheetPickerListItemComponent} from './components/BottomSheetPickerListItemComponent';
+export {default as BottomSheetPickerBoxComponent} from './components/BottomSheetPickerBoxComponent';
