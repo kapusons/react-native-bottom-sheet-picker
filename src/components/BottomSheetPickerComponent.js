ea8e488ff8d98e23701f112772f597e5bd1da762
Lorenzo Angelone <l.angelone@kapusons.it>
2024-10-24 11:49:22 +0200

    forced rerender

--- src/components/BottomSheetPickerComponent.js
+++ src/components/BottomSheetPickerComponent.js
@@ -1,12 +1,18 @@
-import React, {useImperativeHandle} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import React, { useImperativeHandle } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
-import BottomSheetPickerOutlinedTitleComponent from './BottomSheetPickerOutlinedTitleComponent';
-import BottomSheetPickerBoxComponent from './BottomSheetPickerBoxComponent';
-import FormBottomSheetModalComponent from './FormBottomSheetModalComponent';
-import BottomSheetPickerListComponent from './BottomSheetPickerListComponent';
-import {TITLE_FONT_SIZE} from '../constants/font_size_constant';
-import pickerStyleHelper from '../helpers/picker_style_helper';
+import BottomSheetPickerOutlinedTitleComponent from "./BottomSheetPickerOutlinedTitleComponent";
+import BottomSheetPickerBoxComponent from "./BottomSheetPickerBoxComponent";
+import FormBottomSheetModalComponent from "./FormBottomSheetModalComponent";
+import BottomSheetPickerListComponent from "./BottomSheetPickerListComponent";
+import { TITLE_FONT_SIZE } from "../constants/font_size_constant";
+import pickerStyleHelper from "../helpers/picker_style_helper";
 
 const BottomSheetPickerComponent = React.forwardRef((props, ref) => {
   let pickerRef = React.createRef();
@@ -18,136 +24,196 @@ const BottomSheetPickerComponent = React.forwardRef((props, ref) => {
     },
     forceDismiss: () => {
       pickerModalRef.current?.dismiss();
-    }
+    },
+    rerender: () => {
+      rerender();
+    },
   }));
 
   const onSelectItem = (item) => {
     props.onSelectItem(item);
     pickerModalRef.current?.dismiss();
-  }
+  };
 
-  const windowWidth = Dimensions.get('window').width;
-  const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
   const calculatePickerHeight = () => {
-    let h = 425
+    let h = 425;
     if (props.snapPoints && props.snapPoints[0]) {
       try {
-        percentage = parseInt(props.snapPoints[0].split("%")[0])
-        h =  (percentage * windowHeight) / 100
-      } catch {
-      }
+        percentage = parseInt(props.snapPoints[0].split("%")[0]);
+        h = (percentage * windowHeight) / 100;
+      } catch {}
     }
-    return h - 40 ; // 40 "padding bottom"
-  }
+    return h - 40; // 40 "padding bottom"
+  };
 
-  const showPicker = () => {
-    if (props.disabled)
-      return
-
-    !!props.onBottomSheetShow && props.onBottomSheetShow();
-    const content = !!props.customPickerContent ? props.customPickerContent
-                    : <BottomSheetPickerListComponent
-                        title={props.bottomSheetTitle}
-                        items={props.items}
-                        customBottomSheetTitle={props.customBottomSheetTitle}
-                        customListItem={props.customListItem}
-                        listItemStyle={props.listItemStyle}
-                        itemTextStyle={props.itemTextStyle}
-                        pickerContentHeight={props.pickerContentHeight || calculatePickerHeight()}
-                        primaryColor={props.primaryColor}
-                        secondaryColor={props.secondaryColor}
-                        onSelectItem={onSelectItem}
-                        hideListItemAudio={props.hideListItemAudio}
-                        bottomSheetTitleStyle={props.bottomSheetTitleStyle}
-                        selectedItem={props.selectedItem}
-                        showCheckIcon={props.showCheckIcon}
-                        checkIconSize={props.checkIconSize}
-                        titleFontFamily={props.titleFontFamily}
-                        itemFontFamily={props.itemFontFamily}
-                        selectedFieldName={props.selectedFieldName}
-                        showRadioStyle={props.showRadioStyle}
-                        showLeftCheckIcon={props.showLeftCheckIcon}
-                        leftCheckIconColor={props.leftCheckIconColor}
-                        showSubtitle={props.showSubtitle}
-                        subtitleStyle={props.subtitleStyle}
-                        isSearchable={props.isSearchable}
-                        searchPlaceholder={props.searchPlaceholder}
-                        searchInputStyle={props.searchInputStyle}
-                        searchInputContainerStyle={props.searchInputContainerStyle}
-                        searchIconColor={props.searchIconColor}
-                        clearSearchIconColor={props.clearSearchIconColor}
-                        customPreviewItem={props.customPreviewItem}
-                        ListEmptyComponent={props.ListEmptyComponent}
-                        onAddField={props.onAddField}
-                        explanationBox={props.explanationBox}
-                      />
+  const rerender = () => {
+    const content = !!props.customPickerContent ? (
+      props.customPickerContent
+    ) : (
+      <BottomSheetPickerListComponent
+        title={props.bottomSheetTitle}
+        items={props.items}
+        customBottomSheetTitle={props.customBottomSheetTitle}
+        customListItem={props.customListItem}
+        listItemStyle={props.listItemStyle}
+        itemTextStyle={props.itemTextStyle}
+        pickerContentHeight={
+          props.pickerContentHeight || calculatePickerHeight()
+        }
+        primaryColor={props.primaryColor}
+        secondaryColor={props.secondaryColor}
+        onSelectItem={onSelectItem}
+        hideListItemAudio={props.hideListItemAudio}
+        bottomSheetTitleStyle={props.bottomSheetTitleStyle}
+        selectedItem={props.selectedItem}
+        showCheckIcon={props.showCheckIcon}
+        checkIconSize={props.checkIconSize}
+        titleFontFamily={props.titleFontFamily}
+        itemFontFamily={props.itemFontFamily}
+        selectedFieldName={props.selectedFieldName}
+        showRadioStyle={props.showRadioStyle}
+        showLeftCheckIcon={props.showLeftCheckIcon}
+        leftCheckIconColor={props.leftCheckIconColor}
+        showSubtitle={props.showSubtitle}
+        subtitleStyle={props.subtitleStyle}
+        isSearchable={props.isSearchable}
+        searchPlaceholder={props.searchPlaceholder}
+        searchInputStyle={props.searchInputStyle}
+        searchInputContainerStyle={props.searchInputContainerStyle}
+        searchIconColor={props.searchIconColor}
+        clearSearchIconColor={props.clearSearchIconColor}
+        customPreviewItem={props.customPreviewItem}
+        ListEmptyComponent={props.ListEmptyComponent}
+        onAddField={props.onAddField}
+        explanationBox={props.explanationBox}
+      />
+    );
 
     pickerRef.current?.setBodyContent(content);
+  };
+
+  const showPicker = () => {
+    if (props.disabled) return;
+
+    !!props.onBottomSheetShow && props.onBottomSheetShow();
+
+    rerender();
     pickerModalRef.current?.present();
-  }
+  };
 
   const onDismissModal = () => {
     !!props.onDismiss && props.onDismiss();
     pickerRef.current?.setBodyContent(null);
-  }
+  };
 
   const renderPickerTitle = () => {
-    return props.isOutlined ? <BottomSheetPickerOutlinedTitleComponent title={props.title} required={props.required} requiredColor={props.requiredColor} titleFontFamily={props.titleFontFamily}
-                                titleStyle={props.titleStyle} outlinedTitleContainerStyle={props.outlinedTitleContainerStyle} disabled={props.disabled} disabledColor={props.disabledColor} requiredTitleStyle={props.requiredTitleStyle} />
-          : <Text style={[styles.titleLabel, props.titleFontFamily && {fontFamily: props.titleFontFamily}, props.titleStyle]}>
-              {props.title}
-              {props.required && <Text style={[{color: props.requiredColor || "#d50000"}, props.requiredTitleStyle]}> *</Text>}
-            </Text>
-  }
+    return props.isOutlined ? (
+      <BottomSheetPickerOutlinedTitleComponent
+        title={props.title}
+        required={props.required}
+        requiredColor={props.requiredColor}
+        titleFontFamily={props.titleFontFamily}
+        titleStyle={props.titleStyle}
+        outlinedTitleContainerStyle={props.outlinedTitleContainerStyle}
+        disabled={props.disabled}
+        disabledColor={props.disabledColor}
+        requiredTitleStyle={props.requiredTitleStyle}
+      />
+    ) : (
+      <Text
+        style={[
+          styles.titleLabel,
+          props.titleFontFamily && { fontFamily: props.titleFontFamily },
+          props.titleStyle,
+        ]}
+      >
+        {props.title}
+        {props.required && (
+          <Text
+            style={[
+              { color: props.requiredColor || "#d50000" },
+              props.requiredTitleStyle,
+            ]}
+          >
+            {" "}
+            *
+          </Text>
+        )}
+      </Text>
+    );
+  };
 
   return (
-    <View style={[{width: '100%'}, props.containerStyle]}>
-      { !!props.title && renderPickerTitle()}
+    <View style={[{ width: "100%" }, props.containerStyle]}>
+      {!!props.title && renderPickerTitle()}
 
-      <View style={[pickerStyleHelper.getContainerStyleByType(props.isOutlined, props.disabled, props.disabledColor), props.pickerStyle]}>
-        <TouchableOpacity ref={ref} onPress={() => showPicker()} style={{height: '100%'}}>
-          { !!props.customPicker ? props.customPicker
-            : <BottomSheetPickerBoxComponent
-                items={props.items}
-                selectedItem={props.selectedItem}
-                placeholder={props.placeholder}
-                placeholderStyle={props.placeholderStyle}
-                pickerBoxStyle={props.pickerBoxStyle}
-                primaryColor={props.primaryColor}
-                secondaryColor={props.secondaryColor}
-                customIcon={props.customIcon}
-                pickerUuid={props.pickerUuid}
-                placeholderAudio={props.placeholderAudio}
-                playingUuid={props.playingUuid}
-                updatePlayingUuid={(uuid) => props.updatePlayingUuid(uuid)}
-                indicatorLabel={props.indicatorLabel}
-                pickerFontSize={props.pickerFontSize}
-                indicatorLabelStyle={props.indicatorLabelStyle}
-                disabled={props.disabled}
-                disabledColor={props.disabledColor}
-                selectedFieldName={props.selectedFieldName}
-              />
-          }
+      <View
+        style={[
+          pickerStyleHelper.getContainerStyleByType(
+            props.isOutlined,
+            props.disabled,
+            props.disabledColor
+          ),
+          props.pickerStyle,
+        ]}
+      >
+        <TouchableOpacity
+          ref={ref}
+          onPress={() => showPicker()}
+          style={{ height: "100%" }}
+        >
+          {!!props.customPicker ? (
+            props.customPicker
+          ) : (
+            <BottomSheetPickerBoxComponent
+              items={props.items}
+              selectedItem={props.selectedItem}
+              placeholder={props.placeholder}
+              placeholderStyle={props.placeholderStyle}
+              pickerBoxStyle={props.pickerBoxStyle}
+              primaryColor={props.primaryColor}
+              secondaryColor={props.secondaryColor}
+              customIcon={props.customIcon}
+              pickerUuid={props.pickerUuid}
+              placeholderAudio={props.placeholderAudio}
+              playingUuid={props.playingUuid}
+              updatePlayingUuid={(uuid) => props.updatePlayingUuid(uuid)}
+              indicatorLabel={props.indicatorLabel}
+              pickerFontSize={props.pickerFontSize}
+              indicatorLabelStyle={props.indicatorLabelStyle}
+              disabled={props.disabled}
+              disabledColor={props.disabledColor}
+              selectedFieldName={props.selectedFieldName}
+            />
+          )}
         </TouchableOpacity>
       </View>
 
-      <FormBottomSheetModalComponent ref={pickerRef} formModalRef={pickerModalRef} snapPoints={props.snapPoints || ['60%']} onDismissModal={() => onDismissModal()} />
+      <FormBottomSheetModalComponent
+        ref={pickerRef}
+        formModalRef={pickerModalRef}
+        snapPoints={props.snapPoints || ["60%"]}
+        onDismissModal={() => onDismissModal()}
+      />
     </View>
-  )
-})
+  );
+});
 
 const styles = StyleSheet.create({
   titleLabel: {
-    color: 'white',
+    color: "white",
     fontSize: TITLE_FONT_SIZE,
-    marginBottom: 10
+    marginBottom: 10,
   },
 });
 
 export default BottomSheetPickerComponent;
 
 // How to use the component
-{/* <BottomSheetPicker
+{
+  /* <BottomSheetPicker
   title="Your age"   // title on top of the picker box
   placeholder="Select your age"   // placeholder inside the picker box
   bottomSheetTitle="Select you age"   // title on the bottom sheet
@@ -180,4 +246,5 @@ export default BottomSheetPickerComponent;
   playingUuid=''
   updatePlayingUuid={(uuid) => function}
   hideListItemAudio={boolean}
-/> */}
+/> */
+}
